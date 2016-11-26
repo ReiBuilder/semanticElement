88d1666933047b1080936b0974a3d3b2a0ee0228
ReiBuilder <wordtoworld@163.com>
2016-11-26 16:21:39 +0800

    add README.md file

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ myConfigProvider = ConfigProvider.ConfigProvider()
 myWordProvider = WordProvider.WordProvider(myConfigProvider)
 myWordGetter = WordGetter.WordGetter(myWordProvider,myConfigProvider)
 
-# while myWordProvider.getState() != "sleeping":
-#     word = myWordProvider.getNextWord()
-#     myWordGetter.getWordByWebService(word)
-#     time.sleep(1)
+while myWordProvider.getState() != "sleeping":
+    word = myWordProvider.getNextWord()
+    myWordGetter.getWordByWebService(word)
+    time.sleep(1)
 
 print("finish!")
 
